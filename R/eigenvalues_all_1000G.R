d9a8259492fcd21c9f2733af5d5a2e2aefde4126
Tim <timothy.a.myers@gmail.com>
2020-07-30 09:39:06 -0400

    add all eigenvalues

---
+++ R/eigenvalues_all_1000G.R
@@ -0,0 +1,20 @@
+#' Eigenvalues of 2504 individuals from the 1000 Genome Project
+#'
+#' A sample dataset containing the eigenvalues
+#' calculated from 2504 individuals in the Phase 3 data
+#' release of the 1000 Genomes Project. The principal
+#' component analysis was conducted using PLINK.
+#'
+#' @format A data frame with 2504 rows and 1 variable:
+#' \describe{
+#'   \item{eigen_values}{calculated eigennvalues}
+#' }
+#'
+#' @examples
+#'   eigenvalues_1000G
+#' \donttest{genome_values <- eigenvalues
+#'           values <- c(genome_values)$eigen_values
+#' }
+#'
+#' @source {Machiela Lab}
+"eigenvalues_all_1000G"
